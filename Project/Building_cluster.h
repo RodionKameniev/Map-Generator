82dc2c129f4b93bc223c1d5813038eb0c2eadf55
Arsenii-Kameniev <arskamenev@gmail.com>
2026-05-29 12:56:44 +0200

    add std::

--- Project/Building_cluster.h
+++ Project/Building_cluster.h
@@ -4,30 +4,28 @@
 
 #include "Building_component.h"
 
-using namespace std;
-
 class Building_cluster {
 protected:
-    vector<Building_component> building_components;
+    std::vector<Building_component> building_components;
     int cost_of_building;
     float base_probability_to_spawn;
 
 public:
     Building_cluster(
-        const vector<Building_component>& buildings,
+        const std::vector<Building_component>& buildings,
         const int cost_of_building,
         const float base_probability_to_spawn
     );
 
     // Set functions
     void set(
-        const vector<Building_component>& building_components,
+        const std::vector<Building_component>& building_components,
         const int cost_of_building,
         const float base_probability_to_spawn
     );
 
     void set_building_components(
-        const vector<Building_component>& building_components
+        const std::vector<Building_component>& building_components
     );
 
     void set_cost_of_building(
@@ -39,7 +37,7 @@ public:
     );
 
     // Get functions
-    const vector<Building_component>&
+    const std::vector<Building_component>&
         get_building_components() const;
 
     const int get_cost_of_building() const;
