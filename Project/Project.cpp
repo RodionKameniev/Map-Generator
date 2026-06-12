480429961f9fa7152c483f6d22344eac771252af
RodionKameniev <kamenev.r.r@gmail.com>
2026-06-12 20:36:37 +0200

    Sync

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -16,10 +16,26 @@ int main()
     unsigned long long seed = rand();
     int height = 1080;
     int width = 1920;
-    float b = 100.0;
+    float b = 50.0;
+    
+    //Noise creation
+    clock_t tStart = clock();
+
     int* map_in_vales = RunPerlinWindow(seed, width, height, 1.5f*b);
+    
+    printf("Noise taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+    
+    //Values assigment
+    tStart = clock();
+
     Map new_map;
     new_map.create_base_map(map_in_vales, height, width);
+    
+    printf("Map creation time: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+
+    //Render
+    
     new_map.render_map();
+
     return 0;
 }
